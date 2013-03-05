327454f000b268271e7bfe8a12ca94fe254be8f7
kyoe1234@gmail.com <kyoe1234@gmail.com>
2013-03-05 23:13:20 +0900

    샘플파일 인증서 수정

--- passbook/full_sample/index.php
+++ passbook/full_sample/index.php
@@ -2,78 +2,80 @@
 require_once('../PKPass.php');
 
 if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['passenger'])){
-	// User has filled in the flight info, so create the pass now
-	
-	// Predefined data
-	$labels = array(
-		'SFO' => 'San Francisco',
-		'LAX' => 'Los Angeles',
-		'LHR' => 'London'
-	);
-	$gates = array('F12','G43','A2','C5','K9');
-	
-	// User-set vars
-	$passenger = addslashes($_POST['passenger']);
-	$origin = $_POST['origin'];
-	$origin_label = $labels[$origin];
-	$destination = $_POST['destination'];
-	$destination_label = $labels[$destination];
-	$gate = $gates[array_rand($gates)]; // Yup, pick a random gate
-	$date = date('m/d/Y H:i',$_POST['date']); // Convert date to string
-	
-	// Create pass
-	
-	//Set certifivate and path in the constructor
-	$pass = new PKPass('../../Certificate.p12', 'test123'); 
-	
-	// Add the WWDR certificate 
-	$pass->setWWDRcertPath('../AppleWWDR.pem');
+    // User has filled in the flight info, so create the pass now
 
-	//Check if an error occured within the constructor
-	if($pass->checkError($error) == true) {
-		exit('An error occured: '.$error);
-	}
+    // Predefined data
+    $labels = array(
+        'SFO' => 'San Francisco',
+        'LAX' => 'Los Angeles',
+        'LHR' => 'London'
+    );
+    $gates = array('F12','G43','A2','C5','K9');
 
-	//Or do it manually outside of the constructor
-	/*
-	// Set the path to your Pass Certificate (.p12 file)
-	if($pass->setCertificate('../../Certificate.p12') == false) {
-		echo 'An error occured';
-		if($pass->checkError($error) == true) {
-			echo ': '.$error;
-		}
-		exit('.');
-	} 
-	// Set password for certificate
-	if($pass->setCertificatePassword('test123') == false) {
-		echo 'An error occured';
-		if($pass->checkError($error) == true) {
-			echo ': '.$error;
-		}
-		exit('.');
-	}  */
+    // User-set vars
+    $passenger = addslashes($_POST['passenger']);
+    $origin = $_POST['origin'];
+    $origin_label = $labels[$origin];
+    $destination = $_POST['destination'];
+    $destination_label = $labels[$destination];
+    $gate = $gates[array_rand($gates)]; // Yup, pick a random gate
+    $date = date('m/d/Y H:i',$_POST['date']); // Convert date to string
+
+    // Create pass
+
+    //Set certifivate and path in the constructor
+    //$pass = new PKPass('../../Certificate.p12', 'test123');
+    $pass = new PKPass('../certif/ngmsPassTypeId.p12', 'face123');
+
+    // Add the WWDR certificate
+    //$pass->setWWDRcertPath('../AppleWWDR.pem');
+    $pass->setWWDRcertPath('../certif/Apple_Worldwide_Developer.pem');
+
+    //Check if an error occured within the constructor
+    if($pass->checkError($error) == true) {
+        exit('An error occured: '.$error);
+    }
+
+    //Or do it manually outside of the constructor
+    /*
+    // Set the path to your Pass Certificate (.p12 file)
+    if($pass->setCertificate('../../Certificate.p12') == false) {
+        echo 'An error occured';
+        if($pass->checkError($error) == true) {
+            echo ': '.$error;
+        }
+        exit('.');
+    }
+    // Set password for certificate
+    if($pass->setCertificatePassword('test123') == false) {
+        echo 'An error occured';
+        if($pass->checkError($error) == true) {
+            echo ': '.$error;
+        }
+        exit('.');
+    }  */
 
 
-	$pass->setJSON('{ 
-	"passTypeIdentifier": "pass.com.apple.test",
-	"formatVersion": 1,
-	"organizationName": "Flight Express",
-	"serialNumber": "123456",
-	"teamIdentifier": "AGK5BZEN3E",
-	"backgroundColor": "rgb(107,156,196)",
-	"logoText": "Flight info",
-	"description": "Demo pass",
-	"boardingPass": {
+    $pass->setJSON('{
+    "passTypeIdentifier": "pass.skcc.ngms.pushexam",
+    "formatVersion": 1,
+    "organizationName": "Flight Express",
+    "serialNumber": "123456",
+    "teamIdentifier": "NUG3DQ847F",
+    "backgroundColor": "rgb(107,156,196)",
+    "logoText": "Flight info",
+    "description": "Demo pass",
+    "boardingPass": {
         "primaryFields": [
             {
-            	"key" : "origin",
-            	"label" : "'.$origin_label.'",
-            	"value" : "'.$origin.'"
+                "key" : "origin",
+                "label" : "'.$origin_label.'",
+                "value" : "'.$origin.'"
             },
             {
-            	"key" : "destination",
-            	"label" : "'.$destination_label.'",
-            	"value" : "'.$destination.'"
+                "key" : "destination",
+                "label" : "'.$destination_label.'",
+                "value" : "'.$destination.'"
             }
         ],
         "secondaryFields": [
@@ -105,7 +107,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['passenger'])){
     }
     }');
     if($pass->checkError($error) == true) {
-    	exit('An error occured: '.$error);
+        exit('An error occured: '.$error);
     }
 
 
@@ -114,47 +116,47 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['passenger'])){
     $pass->addFile('../images/icon@2x.png');
     $pass->addFile('../images/logo.png');
     if($pass->checkError($error) == true) {
-    	exit('An error occured: '.$error);
+        exit('An error occured: '.$error);
     }
 
-	
-	//If you pass true, the class will output the zip into the browser.
+
+    //If you pass true, the class will output the zip into the browser.
     $result = $pass->create(true);
     if ($result == false) { // Create and output the PKPass
-    	echo $pass->getError();
+        echo $pass->getError();
     }
 
-    
+
 } else {
-	// User lands here, there are no $_POST variables set	
-	?>
-	<html>
-		<head>
-			<title>Flight pass creator - PHP class demo</title>
-			
-			<!-- Reusing some CSS from another project of mine -->
-			<link href="http://www.lifeschool.nl/static/bootstrap.css" rel="stylesheet" type="text/css" />
-			<meta name="viewport" content="width=320; user-scalable=no" />
-			<style>
-				.header { color: white; background-color: #6699cc; padding-top: 30px; padding-bottom: 30px; margin-bottom: 32px; text-align: center; }
-				.logo { width: 64px; height: 64px; margin-bottom: 20px; }
-				.title { color: white; font-size: 22px; text-shadow: 1px 1px 1px rgba(0,0,0,0.5); font-weight: bold; display: block; text-align: center; }
-				.userinfo { margin: 0px auto; padding-bottom: 32px; width: 280px;}
-				form.form-stacked { padding: 0px;}
-				legend { text-align: center; padding-bottom: 25px; clear: both; border-bottom: none;}
-				input.xlarge { width: 280px; height: 26px; line-height: 26px;}
-			</style>
-		</head>
-		<body>
-			<div class="header">
-				<img class="logo" src="icon.png" />
-				<span class="title">Air Company</span>
-			</div>
-			<div class="userinfo">
-				<form action="index.php" method="post" class="form-stacked">
+    // User lands here, there are no $_POST variables set
+    ?>
+    <html>
+        <head>
+            <title>Flight pass creator - PHP class demo</title>
+
+            <!-- Reusing some CSS from another project of mine -->
+            <link href="http://www.lifeschool.nl/static/bootstrap.css" rel="stylesheet" type="text/css" />
+            <meta name="viewport" content="width=320; user-scalable=no" />
+            <style>
+                .header { color: white; background-color: #6699cc; padding-top: 30px; padding-bottom: 30px; margin-bottom: 32px; text-align: center; }
+                .logo { width: 64px; height: 64px; margin-bottom: 20px; }
+                .title { color: white; font-size: 22px; text-shadow: 1px 1px 1px rgba(0,0,0,0.5); font-weight: bold; display: block; text-align: center; }
+                .userinfo { margin: 0px auto; padding-bottom: 32px; width: 280px;}
+                form.form-stacked { padding: 0px;}
+                legend { text-align: center; padding-bottom: 25px; clear: both; border-bottom: none;}
+                input.xlarge { width: 280px; height: 26px; line-height: 26px;}
+            </style>
+        </head>
+        <body>
+            <div class="header">
+                <img class="logo" src="icon.png" />
+                <span class="title">Air Company</span>
+            </div>
+            <div class="userinfo">
+                <form action="index.php" method="post" class="form-stacked">
             <fieldset>
                 <legend style="padding-left: 0px;">Please enter your info</legend>
-                                
+
                 <div class="clearfix">
                     <label style="text-align:left">Flight schedule</label>
                         <div class="input">
@@ -162,7 +164,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['passenger'])){
                                 <option value="SFO" >San Francisco</option>
                                 <option value="LAX" >Los Angeles</option>
                                 <option value="LHR" >Londen</option>
-                            </select> 
+                            </select>
                             &nbsp; to &nbsp;
                             <select name="destination" style="width: auto;">
                                 <option value="SFO" >San Francisco</option>
@@ -171,33 +173,33 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['passenger'])){
                             </select>
                         </div>
                 </div>
-                
+
                 <div class="clearfix">
-                	<label style="text-align:left">Passenger name</label>
-                	<div class="input">
-                		<input class="xlarge" name="passenger" type="text" value="" placeholder="John Appleseed" />
-                	</div>
+                    <label style="text-align:left">Passenger name</label>
+                    <div class="input">
+                        <input class="xlarge" name="passenger" type="text" value="" placeholder="John Appleseed" />
+                    </div>
                 </div>
-                
+
                 <div class="clearfix">
-                	<label style="text-align:left">Flight date</label>
-                	<div class="input">
-                		<select name="date" style="width: 100%;">
+                    <label style="text-align:left">Flight date</label>
+                    <div class="input">
+                        <select name="date" style="width: 100%;">
                                 <option value="<?=time();?>">Today</option>
                                 <option value="<?=(time()+86400);?>">Tomorrow</option>
                                 <option value="<?=(time()+(86400*7));?>">Next week</option>
                             </select>
-                	</div>
+                    </div>
                 </div>
-                
+
                 <br /><br />
                 <center><input type="submit" class="btn primary" value=" Create pass &gt; " /></center>
             </fieldset>
         </form>
 
-			</div>
-		</body>
-	</html>
+            </div>
+        </body>
+    </html>
 <?php
-} 
+}
 ?>
--- passbook/starbucks_sample/index.php
+++ passbook/starbucks_sample/index.php
@@ -1,32 +1,33 @@
 <?php
 if(isset($_POST['name'])){
-	// User has filled in the card info, so create the pass now
-	
-	setlocale(LC_MONETARY, 'en_US');
-	require('../PKPass.php');
-		
-	// Variables
-	$id = rand(100000,999999) . '-' . rand(100,999) . '-' . rand(100,999); // Every card should have a unique serialNumber
-	$balance = '$'.rand(0,30).'.'.rand(10,99); // Create random balance
-	$name = stripslashes($_POST['name']);
-	
-	
-	// Create pass
-	$pass = new PKPass(); 
+    // User has filled in the card info, so create the pass now
 
-	$pass->setCertificate('../../Certificate.p12'); // Set the path to your Pass Certificate (.p12 file)
-	$pass->setCertificatePassword('test123'); // Set password for certificate
-	$pass->setWWDRcertPath('../AppleWWDR.pem');
-	$pass->setJSON('{ 
-	"passTypeIdentifier": "pass.com.apple.test",
-	"formatVersion": 1,
-	"organizationName": "Starbucks",
-	"teamIdentifier": "AGK5BZEN3E",
-	"serialNumber": "'.$id.'",
+    setlocale(LC_MONETARY, 'en_US');
+    require('../PKPass.php');
+
+    // Variables
+    $id = rand(100000,999999) . '-' . rand(100,999) . '-' . rand(100,999); // Every card should have a unique serialNumber
+    $balance = '$'.rand(0,30).'.'.rand(10,99); // Create random balance
+    $name = stripslashes($_POST['name']);
+
+
+    // Create pass
+    $pass = new PKPass();
+
+    //$pass->setCertificate('../../Certificate.p12'); // Set the path to your Pass Certificate (.p12 file)
+    $pass->setCertificate('../certif/ngmsPassTypeId.p12'); // Set the path to your Pass Certificate (.p12 file)
+    $pass->setCertificatePassword('face123'); // Set password for certificate
+    $pass->setWWDRcertPath('../certif/AppleWWDR.pem');
+    $pass->setJSON('{
+    "passTypeIdentifier": "pass.skcc.ngms.pushexam",
+    "formatVersion": 1,
+    "organizationName": "Starbucks",
+    "teamIdentifier": "NUG3DQ847F",
+    "serialNumber": "'.$id.'",
     "backgroundColor": "rgb(240,240,240)",
-	"logoText": "Starbucks",
-	"description": "Demo pass",
-	"storeCard": {
+    "logoText": "Starbucks",
+    "description": "Demo pass",
+    "storeCard": {
         "secondaryFields": [
             {
                 "key": "balance",
@@ -66,51 +67,51 @@ if(isset($_POST['name'])){
         echo 'Error: '.$pass->getError();
     }
     exit;
-	
+
 }else{
-	// User lands here, there are no $_POST variables set	
-	?>
-	<html>
-		<head>
-			<title>Starbucks pass creator - PHP class demo</title>
-			
-			<!-- Reusing some CSS from another project of mine -->
-			<link href="http://www.lifeschool.nl/static/bootstrap.css" rel="stylesheet" type="text/css" />
-			<meta name="viewport" content="width=320; user-scalable=no" />
-			<style>
-				.header { background-color: #CCC; padding-top: 30px; padding-bottom: 30px; margin-bottom: 32px; text-align: center; }
-				.logo { width: 84px; height: 84px; margin-bottom: 20px; }
-				.title { color: black; font-size: 22px; text-shadow: 1px 1px 1px rgba(0,0,0,0.1); font-weight: bold; display: block; text-align: center; }
-				.userinfo { margin: 0px auto; padding-bottom: 32px; width: 280px;}
-				form.form-stacked { padding: 0px;}
-				legend { text-align: center; padding-bottom: 25px; border-bottom: none; clear: both;}
-				input.xlarge { width: 280px; height: 26px; line-height: 26px;}
-			</style>
-		</head>
-		<body>
-			<div class="header">
-				<img class="logo" src="logo_web.png" />
-				<span class="title">Starbucks</span>
-			</div>
-			<div class="userinfo">
-				<form action="index.php" method="post" class="form-stacked">
+    // User lands here, there are no $_POST variables set
+    ?>
+    <html>
+        <head>
+            <title>Starbucks pass creator - PHP class demo</title>
+
+            <!-- Reusing some CSS from another project of mine -->
+            <link href="http://www.lifeschool.nl/static/bootstrap.css" rel="stylesheet" type="text/css" />
+            <meta name="viewport" content="width=320; user-scalable=no" />
+            <style>
+                .header { background-color: #CCC; padding-top: 30px; padding-bottom: 30px; margin-bottom: 32px; text-align: center; }
+                .logo { width: 84px; height: 84px; margin-bottom: 20px; }
+                .title { color: black; font-size: 22px; text-shadow: 1px 1px 1px rgba(0,0,0,0.1); font-weight: bold; display: block; text-align: center; }
+                .userinfo { margin: 0px auto; padding-bottom: 32px; width: 280px;}
+                form.form-stacked { padding: 0px;}
+                legend { text-align: center; padding-bottom: 25px; border-bottom: none; clear: both;}
+                input.xlarge { width: 280px; height: 26px; line-height: 26px;}
+            </style>
+        </head>
+        <body>
+            <div class="header">
+                <img class="logo" src="logo_web.png" />
+                <span class="title">Starbucks</span>
+            </div>
+            <div class="userinfo">
+                <form action="index.php" method="post" class="form-stacked">
             <fieldset>
                 <legend style="padding-left: 0px;">Please enter your info</legend>
-                                                
+
                 <div class="clearfix">
-                	<label style="text-align:left">Nickname</label>
-                	<div class="input">
-                		<input class="xlarge" name="name" type="text" value="Johnny's card" />
-                	</div>
+                    <label style="text-align:left">Nickname</label>
+                    <div class="input">
+                        <input class="xlarge" name="name" type="text" value="Johnny's card" />
+                    </div>
                 </div>
-                                
+
                 <br /><br />
                 <center><input type="submit" class="btn primary" value=" Create pass &gt; " /></center>
             </fieldset>
         </form>
 
-			</div>
-		</body>
-	</html>
-	<?
+            </div>
+        </body>
+    </html>
+    <?
 }
